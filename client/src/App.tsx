f9db7d44a3ee56c0728af8eb93370803bbb14650
iwb0802-sketch <iwb0802@gmail.com>
2026-07-12 07:07:28 +0000

    revert: AI 상담 위젯 스크롤 후 노출 방식으로 복원

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
-          <AiConsultWidget bottomOffset={88} />
+          <AiConsultWidget bottomOffset={88} showAfterScroll />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
